f275d3abf9c4fd3efa59b5c079be447353921fe4
Hines <hineskao@gmail.com>
2021-04-14 20:10:13 -0700

    Switching to a new API for a more stable connection

--- script.js
+++ script.js
@@ -1,66 +1,23 @@
-const quoteContainer = document.getElementById('quote-container');
-const quoteText = document.getElementById('quote');
-const authorText = document.getElementById('author');
-const twitterBtn = document.getElementById('twitter');
-const newQuoteBtn = document.getElementById('new-quote');
-const loader = document.getElementById('loader');
+let apiQuotes = [];
 
-// Show Loading
-function loading() {
-    loader.hidden = false;
-    quoteContainer.hidden = true;
+//Show New Quotes
+function newQuote() {
+    // Pick a random quote from apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote);
 }
 
-// Hide Loading
-function complete() {
-    if (!loader.hidden){
-        quoteContainer.hidden = false;
-        loader.hidden = true;
-    }
-}
-
-// Get Quote From API
-async function getQuote() {
-    loading();
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
-    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+// Get Quotes From API
+async function getQuotes() {
+    const apiUrl = 'https://type.fit/api/quotes';
     try {
-        const response = await fetch(proxyUrl + apiUrl);
-        const data = await response.json();
-        // If Author is blank, add 'Unknown'
-        if (data.quoteAuthor === '') {
-            authorText.innerText = 'Unknown';
-        } else {
-            authorText.innerText = data.quoteAuthor;
-        }
-        //Reduce font size for long quotes
-        if (data.quoteText.length > 120) {
-            quoteText.classList.add('long-quote');
-        } else {
-            quoteText.classList.remove('long-quote')
-        }
-        quoteText.innerText = data.quoteText;
-        // Stop Loader, Show Quote
-        complete();
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
     } catch (error) {
-        getQuote()
-        
+       // Catch Error Here 
     }
 }
-
-
-// Tweet Quote
-function tweetQuote(){
-    const quote = quoteText.innerText;
-    const author = authorText.innerText;
-    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
-    window.open(twitterUrl, '_blank');
-}
-
-// Event Listeners
-newQuoteBtn.addEventListener('click', getQuote);
-twitterBtn.addEventListener('click', tweetQuote);
-
 
 // On Load
-getQuote();
+getQuotes();
